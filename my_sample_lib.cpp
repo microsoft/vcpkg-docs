1f16f575792921176bb5e57390237e35f419bb14
Roy Crihfield <roy@manteia.ltd>
2024-01-24 06:33:54 +0800

    Fix `greet` to match tutorial (#233)

--- my_sample_lib.cpp
+++ my_sample_lib.cpp
@@ -1,6 +1,6 @@
 #include "my_sample_lib.h"
 #include <fmt/core.h>
 
-std::string greet(const std::string& name) {
-    return fmt::format("Hello, {}!", name);
+void greet(const std::string& name) {
+    fmt::print("Hello, {}!\n", name);
 }
